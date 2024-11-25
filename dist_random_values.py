b5afee66d5ef8c8ff9ac589dd231e7d720933ec5
Joao Sabino <zambino.ueca@gmail.com>
2024-11-25 13:03:38 +0000

    Improved code performance

--- dist_random_values.py
+++ dist_random_values.py
@@ -2,30 +2,62 @@ def dist_random_values(nvals, vals, probs):
 
     import numpy as np
 
+    tol = 1e-6
+    #trick_value = tol*tol#np.float64(10) # dirty trick
+
     if len(vals) != len(probs):
         print("List of values and probabilities must have the same size!")
 
         return ValueError
     
-    if sum(probs) != 1:
+    if abs(1 - sum(probs)) > tol:
         print("Probabilities must sum to 1!")
 
         return ValueError
     
     val_array = np.random.uniform(size=nvals)
+    val_array_store = np.zeros(nvals)
 
-    idx_ordered = np.argsort(probs)
-    porder = probs[idx_ordered]
-    vorder = vals[idx_ordered]
+    # print("SHAPES")
+    # print(val_array.shape)
+    # print(val_array_store.shape)
 
+    idx_ordered = np.argsort(probs, stable=True)
+    porder = probs[idx_ordered.astype(int)]
+    vorder = vals[idx_ordered.astype(int)]
 
+    #index_prev = None
     for idx, val in enumerate(vorder):
-        if idx == 0:
-            indexes = np.where(val_array <= porder[idx])[0]
-            val_array[indexes] = val
-            continue
+
+        #print("IDX: " + str(idx))
         
-        indexes = np.where(np.logical_and(val_array > porder[idx-1], val_array <= porder[idx] + porder[idx-1]))[0]
-        val_array[indexes] = val
-    
-    return val_array
+        if idx == 0:
+            upper = porder[idx]
+            indexes = np.where(val_array <= upper)[0]
+            # index_prev = indexes
+            val_array_store[indexes] = val #+ trick_value # dirty trick
+            continue
+
+
+        lower = upper
+        upper = porder[idx] + porder[:idx].sum()
+        #print("Lower: " + str(lower) + " Uper: " +str(upper))
+        aaa = np.logical_and(val_array > lower, val_array <= upper)
+        #print("val_array: ")
+        #print(val_array)
+        #print("And op: ")
+        #print(aaa)
+        indexes = np.where(np.logical_and(val_array > lower, val_array <= upper))[0]
+        # print(np.intersect1d(indexes, index_prev).shape)
+        # print("Indexes len: " +str(indexes.shape))
+        # print("Val: " + str(val))
+        # print("Check 1: "+str(val_array[val_array==(val)].shape))
+        val_array_store[indexes] = val# +  trick_value # trick to avoid wrong replacement. Needs a better implementation
+        #print("Check 2: "+str(val_array[val_array==(val)].shape))
+        # index_prev = indexes
+
+
+    # [print(val_array[val_array==vvv].shape[0]/val_array.shape[0]) for vvv in vorder]
+    return val_array_store# - trick_value
+
+# Test if it works without trick value (why is val_cnt==0?
--- qtx.py
+++ qtx.py
@@ -2,12 +2,13 @@ import numpy as np
 import qutip as qtp
 import matplotlib.pyplot as plt
 import time
+from dist_random_values import dist_random_values
 
 ###--- Variables
 t0 = time.time()
 # Time definitions
 sim_clock = 1
-src_clock = 1e-3
+src_clock = 1e-6
 sim_time  = 5*sim_clock
 
 Nevents = int(sim_time/src_clock)
@@ -18,10 +19,14 @@ muS = 0.6
 muD = 0.1
 muV = 0.005
 
+mus = np.array([muS, muD, muV])
+
 # ###--- Probability of signal, decoy and vacuum states
-pV = 0.005
 pD = 0.1
-pS = 1 - pD - pV 
+pV = 0.005
+pS = 1 - pD - pV
+
+probs = np.array([pS, pD, pV])
 
 
 ###--- Generate basis, plarizations and intensities
@@ -31,40 +36,28 @@ pols = np.random.randint(0, high=2, size=Nevents) # Generates string of zeros an
 
 basis_pol = np.random.randint(0, high=2, size=Nevents) # Generates string of zeros and ones to choose the basis (convention: 0-HV, 1-AD)
 
-
 intensities = np.random.uniform(size=Nevents)
 
-# Vacuum intensities
-indexV = np.where(intensities <= pV)[0]
-totV = indexV.shape[0] # Total number of Vaccuum states
-intensities[indexV] = muV
 
-# Decoy intensities
-indexD = np.where(np.logical_and(intensities > pV, intensities <= pD+pV))[0]
-totD = indexD.shape[0] # Total number of Decoy states
-intensities[indexD] = muD
-
-# Signal intensities
-indexS = np.where(intensities > pD+pV)[0]
-totS = indexS.shape[0] # Total number of Signal states
-intensities[indexS] = muS
-
-
+#--- New way - The 'dist_random_values' way:
+intensities = dist_random_values(Nevents, mus, probs)
+unique, counts = np.unique(intensities, return_counts=True)
+val_cnt = np.array([len(intensities[intensities == mu]) for mu in mus])
 
 ###--- Generate the coherent states
-dim = 5 # Fock state dimnsion
+dim = 6 # Fock state dimnsion
 
-# Vaccum Coherent State
-alpha = np.sqrt(muV) # alpha could be complex but I'm keeping it simple for now
-stateV = qtp.coherent(dim, alpha)
+# # Vaccum Coherent State
+# alpha = np.sqrt(muV) # alpha could be complex but I'm keeping it simple for now
+# stateV = qtp.coherent(dim, alpha)
 
-# Decoy Coherent State
-alpha = np.sqrt(muD) # alpha could be complex but I'm keeping it simple for now
-stateD = qtp.coherent(dim, alpha)
+# # Decoy Coherent State
+# alpha = np.sqrt(muD) # alpha could be complex but I'm keeping it simple for now
+# stateD = qtp.coherent(dim, alpha)
 
-# Signal Coherent State
-alpha = np.sqrt(muS) # alpha could be complex but I'm keeping it simple for now
-stateS = qtp.coherent(dim, alpha)
+# # Signal Coherent State
+# alpha = np.sqrt(muS) # alpha could be complex but I'm keeping it simple for now
+# stateS = qtp.coherent(dim, alpha)
 
 
 ###--- Compute how many photons we will have on each instant
@@ -74,21 +67,22 @@ For this, I will check how many times we have to measure each coherent state
 coherent states that many times. This will give us the amount of photons for each pulse.
 """
 
-# Generate measurement operators
 measurement_ops = [qtp.ket2dm(qtp.basis(dim, _)) for _ in range(dim)]
 
-#resultV, _ = qtp.measurement.measure(stateV, measurement_ops)
-resultV = np.array([qtp.measurement.measure(stateV, measurement_ops)[0] for res in range(0, totV)])
-#resultD, _ = qtp.measurement.measure(stateD, measurement_ops)
-resultD = np.array([qtp.measurement.measure(stateD, measurement_ops)[0] for res in range(0, totD)])
-#resultS, _ = qtp.measurement.measure(stateS, measurement_ops)
-resultS = np.array([qtp.measurement.measure(stateS, measurement_ops)[0] for res in range(0, totS)])
+alpha = np.array([qtp.coherent(dim, mu) for mu in  mus])
+measure_probs = [qtp.measurement.measurement_statistics_povm(state, measurement_ops)[1] for state in alpha]
+
+# stats = [np.array(list(zip(range(dim), measure))) for measure in measure_probs]
+
+#val_cnt = np.array([len(intensities[intensities == mu]) for mu in mus])
+results = [dist_random_values(val_cnt[idx], np.arange(dim), np.array(measure_probs[idx])) for idx in range(len(measure_probs))]
 
 nphotons = np.zeros(intensities.shape)
 
-nphotons[indexV] = resultV
-nphotons[indexD] = resultD
-nphotons[indexS] = resultS
+for idx, res in enumerate(results):
+    nphotons[np.where(intensities==mus[idx])[0]] = res
+    # nphotons[np.where(intensities==unique[idx])[0]] = res
+
 
 ### Generate polarization states - TODO
 polH = np.array([[1], [0]])#qtp.basis(2, 0)
@@ -114,18 +108,12 @@ print("Simulation took " + str(t1 - t0) + " seconds to run.")
 
 
 
-
-
-
-
-
-
 # Control Report
 
 print("Pulse percentage: ")
-print("muS: " + str(len(indexS)/Nevents))
-print("muD: " + str(len(indexD)/Nevents))
-print("muV: " + str(len(indexV)/Nevents))
+print("muS: " + str(val_cnt[0]/Nevents))
+print("muD: " + str(val_cnt[1]/Nevents))
+print("muV: " + str(val_cnt[2]/Nevents))
 
 def poisson(mu, kk):
     from scipy.special import factorial
@@ -140,41 +128,11 @@ poiS = poisson(muS, xx)
 
 plt.rcParams['text.usetex'] = True
 height = 0.8 # heigh of text in plot
-#--- Vaccum pulses dist
-# plt.subplot(1, 3, 1)
-# plt.plot(xx, poiV*Nevents, '--r')
-# vals, bins, bars = plt.hist(resultV, bins=[0,1,2,3,4,5])
-# plt.ylabel(r"Absolute frequency")
-# plt.ylim((0, Nevents*muS))
-# plt.bar_label(bars, fontsize= 10, color=[0,0,0,0.5])
-# plt.text(dim/2, height*muS*Nevents, r"$\mu = " + str(muV) + "$")
-# # plt.yscale("log")
-
-# #--- Vaccum pulses dist
-# plt.subplot(1, 3, 2)
-# plt.plot(xx, poiD*Nevents, '--r')
-# vals, bins, bars = plt.hist(resultD, bins=[0,1,2,3,4,5])
-# plt.ylim((0, Nevents*muS))
-# plt.xlabel(r"\# photons")
-# plt.yticks([])
-# plt.bar_label(bars, fontsize= 10, color=[0,0,0,0.5])
-# plt.text(dim/2, height*muS*Nevents, r"$\mu = " + str(muD) + "$")
-# # plt.yscale("log")
-
-# #--- Vaccum pulses dist
-# plt.subplot(1, 3, 3)
-# plt.plot(xx, poiS*Nevents, '--r')
-# vals, bins, bars = plt.hist(resultS, bins=[0,1,2,3, 4, 5])
-# plt.ylim((0, Nevents*muS))
-# plt.yticks([])
-# plt.bar_label(bars, fontsize= 10, color=[0,0,0,0.5])
-# plt.text(dim/2, height*muS*Nevents, r"$\mu = " + str(muS) + "$")
-# # plt.yscale("log")
 
 
 fig, axs = plt.subplots(2, 3)
 axs[0, 0].plot(xx, poiV*Nevents, '--r')
-vals, bins, bars = axs[0, 1].hist(resultV, bins=[0,1,2,3,4,5])
+vals, bins, bars = axs[0, 1].hist(results[2], bins=[0,1,2,3,4,5])
 axs[0, 0].set_ylabel(r"Absolute frequency")
 axs[0, 0].set_ylim((0, Nevents*muS))
 axs[0, 0].bar_label(bars, fontsize= 10, color=[0,0,0,0.5])
@@ -184,7 +142,7 @@ axs[0, 0].text(dim/2, height*muS*Nevents, r"$\mu = " + str(muV) + "$")
 #--- Vaccum pulses dist
 
 axs[0, 1].plot(xx, poiD*Nevents, '--r')
-vals, bins, bars = axs[0, 1].hist(resultD, bins=[0,1,2,3,4,5])
+vals, bins, bars = axs[0, 1].hist(results[1], bins=[0,1,2,3,4,5])
 axs[0, 1].set_ylim((0, Nevents*muS))
 axs[0, 1].set_xlabel(r"\# photons")
 axs[0, 1].set_yticks([])
@@ -195,7 +153,7 @@ axs[0, 1].text(dim/2, height*muS*Nevents, r"$\mu = " + str(muD) + "$")
 #--- Vaccum pulses dist
 
 axs[0, 2].plot(xx, poiS*Nevents, '--r')
-vals, bins, bars = axs[0, 2].hist(resultS, bins=[0,1,2,3, 4, 5])
+vals, bins, bars = axs[0, 2].hist(results[0], bins=[0,1,2,3, 4, 5])
 axs[0, 2].set_ylim((0, Nevents*muS))
 axs[0, 2].set_yticks([])
 axs[0, 2].bar_label(bars, fontsize= 10, color=[0,0,0,0.5])
@@ -216,6 +174,3 @@ plt.show()
 
 
 
-###--- TODO
-# - Write function dist_random_values([v1,v2,...,vn], [p1, p2, ..., pn])
-# - test times
